ded27145519bd00210824b25f1500c7014211d21
eltimirstoilkov <eltimir.stoilkov.highschool@svetlina.net>
2023-01-12 13:53:19 +0200

    Update citiesSQL.sql

--- danni/citiesSQL.sql
+++ danni/citiesSQL.sql
@@ -1,4 +1,13 @@
-INSERT into cities (city) VALUES('SOFIA')
-INSERT into cities (city) VALUES('BURGAS')
-INSERT into cities (city) VALUES('STARA ZAGORA')
+INSERT into cities (city) VALUES(N'София')
+INSERT into cities (city) VALUES(N'Бургас')
+INSERT into cities (city) VALUES(N'Кюстендил')
+INSERT into cities (city) VALUES(N'Велико Търново')
+INSERT into cities (city) VALUES(N'Варна')
+INSERT into cities (city) VALUES(N'Пловдив')
+INSERT into cities (city) VALUES(N'Русе')
+INSERT into cities (city) VALUES(N'Благоевград')
+INSERT into cities (city) VALUES(N'Стара Загора')
+INSERT into cities (city) VALUES(N'други')
+
+
  
